4ce312003097437e01a19fb4f8713d756b92c847
Ezequiel-Lima <joao.lima94@fatec.sp.gov.br>
2021-12-12 11:49:15 -0300

    Add project files.

---
+++ Exercicio 9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos deseja alugar? ");
+            int quantidadeDeQuartos = int.Parse(Console.ReadLine());
+            
+            Quarto[] aluguel = new Quarto[quantidadeDeQuartos];
+
+            for (int i = 0; i < quantidadeDeQuartos; i++)
+            {
+                Console.WriteLine($"\nAluguel #{i + 1}");
+                Console.Write("Name: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                aluguel[i] = new Quarto(nome, email, quarto);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < quantidadeDeQuartos; i++)
+            {
+                if (aluguel[i] != null)
+                {
+                    Console.WriteLine($"{aluguel[i]}");
+                }
+            }
+
+            Console.WriteLine("\nPressione qualquer tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio 9/Quarto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_9
+{
+    internal class Quarto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int NumeroDoQuarto { get; set; }
+
+        public Quarto(string name, int numeroDoQuarto)
+        {
+            Name = name;
+            NumeroDoQuarto = numeroDoQuarto;
+        }
+
+        public Quarto(string name, string email, int numeroDoQuarto) : this (name, numeroDoQuarto)
+        { 
+            Email = email;         
+        }
+
+        public override string ToString()
+        {
+            return NumeroDoQuarto +
+                ": " +
+                Name +
+                ", " +
+                Email;
+        }
+    }
+}
